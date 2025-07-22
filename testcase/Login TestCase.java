7642d07fe6deb2ff2236474db45090a1e54df0bf
Ragini Ashwini Arvind Kamble <ragini.ak3940@gmail.com>
2025-07-22 09:24:55 +0530

    Update Login TestCase.java

--- testcase/Login TestCase.java
+++ testcase/Login TestCase.java
@@ -1,8 +1,7 @@
 @Test
 public void registerUser() {
     driver.findElement(By.linkText("Signup / Login")).click();
-    driver.findElement(By.name("name")).sendKeys("TestUser");
-    driver.findElement(By.name("email")).sendKeys("testuser@example.com");
+    driver.findElement(By.name("name")).sendKeys("Ragini");
+    driver.findElement(By.name("email")).sendKeys("ragini.ak3940@gmail.com");
     driver.findElement(By.xpath("//button[text()='Signup']")).click();
-    // Add more form details & assertions
 }
