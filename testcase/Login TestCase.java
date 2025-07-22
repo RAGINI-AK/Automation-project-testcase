f897018a02949949047d5e57c81bb1342ff69b0b
Ragini Ashwini Arvind Kamble <ragini.ak3940@gmail.com>
2025-07-22 09:38:52 +0530

    Delete testcase directory

--- testcase/Login TestCase.java
+++
@@ -1,7 +0,0 @@
-@Test
-public void registerUser() {
-    driver.findElement(By.linkText("Signup / Login")).click();
-    driver.findElement(By.name("name")).sendKeys("Ragini");
-    driver.findElement(By.name("email")).sendKeys("ragini.ak3940@gmail.com");
-    driver.findElement(By.xpath("//button[text()='Signup']")).click();
-}
